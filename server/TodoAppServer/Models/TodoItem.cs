5bd1d6447b8608961a389e37a42bcc9ae3f59286
Kaja <karolinakuchynova@seznam.cz>
2023-04-17 10:44:49 +0200

    Add model and repository

---
+++ server/TodoAppServer/Models/TodoItem.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoAppServer.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string? Name { get; set; }
+
+        [Required]
+        public int? Priority { get; set; }
+
+        [Required]
+        public TodoItemStatus? Status { get; set; }
+    }
+
+    public enum TodoItemStatus
+    {
+        NotStarted,
+        InProgress,
+        Completed
+    }
+}
---
+++ server/TodoAppServer/Repositories/TodoItemRepository.cs
@@ -0,0 +1,42 @@
+﻿using TodoAppServer.Models;
+
+namespace TodoAppServer.Repositories
+{
+    public class TodoItemRepository : ITodoItemRepository
+    {
+        private int _nextId = 1;
+        private Dictionary<int, TodoItem> _todos = new Dictionary<int, TodoItem>();
+
+        /// <inheritdoc />
+        public TodoItem? GetTodoById(int id) {
+            _todos.TryGetValue(id, out TodoItem? item);
+            return item;
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<TodoItem> GetAllTodos() {
+            return _todos.Values.ToList();
+        }
+
+        /// <inheritdoc />
+        public TodoItem InsertTodo(TodoItem item) {
+            item.Id = _nextId;
+            _todos.Add(item.Id, item);
+            _nextId++;
+
+            return item;
+        }
+
+        // <inheritdoc />
+        public void UpdateTodo(TodoItem item) {
+            if (_todos.ContainsKey(item.Id)) {
+                _todos[item.Id] = item;
+            }
+        }
+
+        // <inheritdoc />
+        public void DeleteTodo(int todoId) {
+            _todos.Remove(todoId);
+        }
+    }
+}
